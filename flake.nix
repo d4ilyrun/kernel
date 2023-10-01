b160fdaef5133d893f8f5b9c9e66cfff2036ff22
Léo DUBOIN <leo@duboin.com>
2023-10-02 01:09:52 +0200

    feat(flake): add cross compilation environment

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+
+    nixpkgs = {
+      type = "github";
+      owner = "NixOs";
+      repo = "nixpkgs";
+      ref = "23.05";
+    };
+
+    flake-utils = {
+      type = "github";
+      owner = "numtide";
+      repo = "flake-utils";
+    };
+
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        pkgs-i686 = pkgs.pkgsCross.i686-embedded;
+      in
+      {
+        devShells = rec {
+          default = kernel;
+          kernel = pkgs.mkShell.override { inherit (pkgs-i686) stdenv; } { };
+        };
+      }
+    );
+}
