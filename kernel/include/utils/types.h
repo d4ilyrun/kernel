aeca3e08181d702707344b55c3b9e86da512aa08
Léo DUBOIN <leo@duboin.com>
2023-10-23 22:19:08 +0200

    feat(kernel: utils): define type aliases

---
+++ kernel/include/utils/types.h
@@ -0,0 +1,14 @@
+#ifndef UTILS_TYPES_H
+#define UTILS_TYPES_H
+
+#include <stdint.h>
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+
+typedef int8_t s8;
+typedef int16_t s16;
+typedef int32_t s32;
+
+#endif /* UTILS_TYPES_H */
