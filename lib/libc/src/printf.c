181f0c44816732b2ee80676b6fc1382e4d4d1b5a
Léo DUBOIN <leo@duboin.com>
2023-10-31 01:08:32 +0100

    feat(libc: printf): add length modifiers

--- lib/libc/src/printf.c
+++ lib/libc/src/printf.c
@@ -1,11 +1,13 @@
 #include <kernel/devices/uart.h>
 
 #include <stdarg.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <utils/compiler.h>
+#include <utils/types.h>
 
 static_assert(sizeof(int) == sizeof(long),
-              "Invalid architecture: sizeof(long) != sizeof(int)");
+              "Unsupported architecture: sizeof(long) != sizeof(int)");
 
 #define TOK_DELIMITER '%'
 
@@ -19,6 +21,11 @@ static_assert(sizeof(int) == sizeof(long),
 #define TOK_POINTER 'p'
 #define TOK_BINARY 'b' // custom token
 #define TOK_ESCAPE TOK_DELIMITER
+
+#define TOK_LEN_ELL 'l'
+#define TOK_LEN_SHORT 'h'
+#define TOK_LEN_SIZE_T 'z'
+#define TOK_LEN_PTRDIFF_T 't'
 
 #define MAXBUF (sizeof(long) * 8) // 32B max (times 8 bits)
 
@@ -41,7 +48,7 @@ static void printf_puts(register char *str, int *written)
         printf_char(*str++, written);
 }
 
-static void printf_utoa_base(register unsigned int x,
+static void printf_utoa_base(register unsigned long long x,
                              register unsigned int base, int *written)
 {
     static const char digits[] = "0123456789abcdef";
@@ -68,34 +75,114 @@ static void printf_itoa(register int x, int *written)
     }
 }
 
-static void printf_step(char c, int *index, int *written, va_list *parameters)
+typedef struct printf_ctx {
+    char ell;     // Number of 'ell' before the current token
+    char h;       // Number of 'h' before the current token
+    char z;       // Following conversion corresponds to a (s)size_t
+    char t;       // Following conversion corresponds to a ptrdiff_t
+    char invalid; // Ill formed contex
+} printf_ctx;
+
+static printf_ctx printf_length_modifiers(const char *format, int *index)
+{
+    printf_ctx ctx = {0};
+
+    switch (format[*index]) {
+    case TOK_LEN_ELL:
+        while (format[(*index)++] == TOK_LEN_ELL)
+            ctx.ell += 1;
+        *index -= 1; // skipped over the next character
+        return ctx;
+
+    case TOK_LEN_SHORT:
+        while (format[(*index)++] == TOK_LEN_SHORT)
+            ctx.h += 1;
+        *index -= 1; // skipped over the next character
+        return ctx;
+
+    case TOK_LEN_SIZE_T:
+
+        ctx.z = 1;
+        break;
+
+    case TOK_LEN_PTRDIFF_T:
+        ctx.t = 1;
+        break;
+
+    case 'q':
+        ctx.ell = 2;
+        break;
+
+    case '\0':
+        ctx.invalid = 1;
+        return ctx;
+
+    default:
+        return ctx;
+    }
+
+    *index += 1;
+
+    return ctx;
+}
+
+static void print_unsigned(register int base, va_list *parameters,
+                           const printf_ctx *ctx, int *written)
+{
+    // Here we know that sizeof(int) == sizeof(long) anyway and skip the
+    // case were we only have a single 'l' modifier (cf. static_assert)
+    if (ctx->ell >= 2)
+        printf_utoa_base(va_arg(*parameters, unsigned int), base, written);
+    else if (ctx->h == 1)
+        printf_utoa_base((unsigned short)va_arg(*parameters, unsigned int),
+                         base, written);
+    else if (ctx->h >= 2)
+        printf_utoa_base((unsigned char)va_arg(*parameters, unsigned int), base,
+                         written);
+    else if (ctx->z)
+        printf_utoa_base(va_arg(*parameters, size_t), base, written);
+    else if (ctx->t)
+        printf_utoa_base(va_arg(*parameters, ptrdiff_t), base, written);
+    else
+        printf_utoa_base(va_arg(*parameters, unsigned long long), base,
+                         written);
+}
+
+static int printf_step(char c, int *written, va_list *parameters,
+                       const printf_ctx *ctx)
 {
     switch (c) {
+
+    // Here we assume that sizeof(int) == sizeof(long) anyway and skip the
+    // case were we only have a single 'l' modifier (cf. static_assert)
     case TOK_DECIMAL:
-        printf_itoa(va_arg(*parameters, int), written);
+        if (ctx->ell < 2)
+            printf_itoa(va_arg(*parameters, int), written);
+        else if (ctx->z)
+            printf_itoa(va_arg(*parameters, ssize_t), written);
+        else if (ctx->t)
+            printf_itoa(va_arg(*parameters, ptrdiff_t), written);
+        else
+            printf_itoa(va_arg(*parameters, long long), written);
         break;
 
     case TOK_UNSIGNED:
-        printf_utoa_base(va_arg(*parameters, unsigned int), 10, written);
+        print_unsigned(10, parameters, ctx, written);
         break;
 
     case TOK_OCTAL:
-        printf_char('0', written);
-        printf_utoa_base(va_arg(*parameters, unsigned int), 8, written);
+        print_unsigned(8, parameters, ctx, written);
         break;
 
     case TOK_HEX:
-        printf_puts("0x", written);
-        printf_utoa_base(va_arg(*parameters, unsigned int), 16, written);
+        print_unsigned(16, parameters, ctx, written);
         break;
 
     case TOK_BINARY:
-        printf_utoa_base(va_arg(*parameters, unsigned int), 2, written);
-        printf_char('b', written);
+        print_unsigned(2, parameters, ctx, written);
         break;
 
     case TOK_POINTER:
-        printf_puts("0x", written);
         printf_utoa_base((unsigned int)va_arg(*parameters, void *), 16,
                          written);
         break;
@@ -113,36 +200,47 @@ static void printf_step(char c, int *index, int *written, va_list *parameters)
         break;
 
     default:
-        printf_char(TOK_DELIMITER, written);
-        *index -= 1;
-        break;
+        return -1;
     }
 
-    *index += 1;
+    return 0;
 }
 
 int printf(const char *format, ...)
 {
-    if (format == 0)
-        return 0;
-
     va_list parameters;
     int written = 0;
+    int error = 0;
 
     va_start(parameters, format);
 
-    for (int i = 0; format[i]; ++i) {
-        char c = format[i];
+    int i = 0;
+    while (format[i] != '\0') {
 
+        char c = format[i++];
+
+        // Also print in case we reached the end of the format string
         if (c != TOK_DELIMITER) {
             printf_char(c, &written);
             continue;
         }
 
-        printf_step(format[i + 1], &i, &written, &parameters);
+        const printf_ctx ctx = printf_length_modifiers(format, &i);
+        if (ctx.invalid) {
+            error = 1;
+            continue;
+        }
+
+        if (printf_step(format[i], &written, &parameters, &ctx) == -1) {
+            printf_char(format[i], &written);
+            error = 1;
+            continue;
+        }
+
+        i += 1;
     }
 
     va_end(parameters);
 
-    return written;
+    return error ? -1 : written;
 }
