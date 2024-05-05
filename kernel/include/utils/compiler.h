9cee962c76d7f42e106de50b576319677c045256
Léo DUBOIN <leo@duboin.com>
2024-05-05 03:35:43 +0200

    chore(kernel: utils): display failed assertion in static_assert

--- kernel/include/utils/compiler.h
+++ kernel/include/utils/compiler.h
@@ -1,9 +1,12 @@
 #ifndef UTILS_COMPILER_H
 #define UTILS_COMPILER_H
 
+#include "stringify.h"
+
 #define ASM __asm__ volatile
 
-#define static_assert _Static_assert // NOLINT
+#define static_assert(cond, ...) \
+    _Static_assert(cond, stringify(cond) __VA_OPT__(": ") __VA_ARGS__)
 
 #define ALWAYS_INLINE __attribute__((always_inline)) inline
 #define PACKED __attribute__((__packed__))
