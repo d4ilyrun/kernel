9f62adf3c193b27ffea542fb4c800fe6a651ac88
Léo DUBOIN <leo@duboin.com>
2024-10-21 17:41:49 +0200

    feat(utils): add endianness swapping functions

--- include/arch.h
+++ include/arch.h
@@ -3,9 +3,14 @@
 #if defined(__i386__)
 #define ARCH_IS_32_BITS
 #define ARCH_WORD_SIZE 4
+#define ARCH_LITTLE_ENDIAN
 #elif defined(__x86_64__)
 #define ARCH_IS_64_BITS
 #define ARCH_WORD_SIZE 8
 #else
 #error Unsupported CPU architecture
+#endif
+
+#ifndef ARCH_LITTLE_ENDIAN
+#error Unsupported endianness
 #endif
--- include/utils/bits.h
+++ include/utils/bits.h
@@ -28,6 +28,43 @@
 /** Read the nth bit */
 #define BIT_READ(_x, _n) ((_x)&BIT((_n)))
 
+static inline uint16_t __bswap_16(uint16_t x)
+{
+    return (x << 8) | (x >> 8);
+}
+
+static inline uint32_t __bswap_32(uint32_t x)
+{
+    return (x << 24) | (x & 0x0000FF00 << 8) | (x & 0x00FF0000 >> 8) |
+           (x >> 24);
+}
+
+#ifdef ARCH_LITTLE_ENDIAN
+
+#define htobe16(x) __bswap_16(x)
+#define htole16(x) (uint16_t)(x)
+#define be16toh(x) __bswap_16(x)
+#define le16toh(x) (uint16_t)(x)
+
+#define htobe32(x) __bswap_32(x)
+#define htole32(x) (uint32_t)(x)
+#define be32toh(x) __bswap_32(x)
+#define le32toh(x) (uint32_t)(x)
+
+#else
+
+#define htobe16(x) (uint16_t)(x)
+#define htole16(x) __bswap_16(x)
+#define be16toh(x) (uint16_t)(x)
+#define le16toh(x) __bswap_16(x)
+
+#define htobe32(x) (uint32_t)(x)
+#define htole32(x) __bswap_32(x)
+#define be32toh(x) (uint32_t)(x)
+#define le32toh(x) __bswap_32(x)
+
+#endif
+
 /** @brief Find the index of the last set bit inside @c word */
 static ALWAYS_INLINE unsigned long bit_first_one(unsigned long word)
 {
