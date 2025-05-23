0e7b71df9757ceb7541ea3005f1bf94dbf1917d1
Léo DUBOIN <leo@duboin.com>
2025-05-23 22:01:08 +0200

    fix(sysroot: string.h): add strlcpy back into string.h
    We removed it in de2cce0 because it would not compile, but it is
    necessary to compile when using a toolchain without a libc.

--- toolchain/sysroot/usr/include/string.h
+++ toolchain/sysroot/usr/include/string.h
@@ -6,6 +6,7 @@
 size_t strlen(const char *s);
 
 char *strncpy(char *dst, const char *src, size_t);
+size_t strlcpy(char *dst, const char *src, size_t);
 
 void *memset(void *s, int c, size_t n);
 void *memcpy(void *restrict dest, const void *restrict src, size_t n);
