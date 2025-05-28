2327132eef4a922d1b7ed5e795a1e4c216d8a620
Léo DUBOIN <leo@duboin.com>
2025-05-28 19:38:12 +0200

    feat(toolchain: newlib): implement fork syscall

--- toolchain/newlib/port/newlib/libc/sys/dailyrun/syscalls.c
+++ toolchain/newlib/port/newlib/libc/sys/dailyrun/syscalls.c
@@ -6,13 +6,25 @@
 #include <sys/times.h>
 #include <sys/types.h>
 
+/* TODO: Expose syscall numbers through uapi headers */
+
 char **environ; /* pointer to array of char * strings that define the current
                    environment variables */
 
+#define DEFINE_SYSCALL_0(_ret_type, _syscall, _nr)                      \
+    _ret_type _syscall(void)                                            \
+    {                                                                   \
+        int ret = _nr;                                                  \
+        __asm__ volatile("int 0x80" : "=a"(ret) : "a"(ret) : "memory"); \
+        return ret;                                                     \
+    }
+
+DEFINE_SYSCALL_0(int, fork, 2);
+
+/* Unimplemented syscalls */
 void _exit(int val);
 int close(int file);
 int execve(char *name, char **argv, char **env);
-int fork(void);
 int fstat(int file, struct stat *st);
 int getpid();
 int isatty(int file);
