496ce4cb579f98ba8d4941897d4fbb02fdde7c34
Léo DUBOIN <leo@duboin.com>
2025-06-16 08:26:30 +0200

    feat(include: types): define our own size_t size

--- include/uapi/sys/types.h
+++ include/uapi/sys/types.h
@@ -1,10 +1,18 @@
 #ifndef UAPI_SYS_TYPES_H
 #define UAPI_SYS_TYPES_H
 
-#include <stdint.h>
+/*
+ * Redefine our own size_t type.
+ *
+ * This is needed to avoid LSP errors caused by a conflict between
+ * our compiler and the one used by clangd.
+ */
+#ifdef __SIZE_TYPE__
+#undef __SIZE_TYPE__
+#endif
+#define __SIZE_TYPE__ unsigned long int
 
 typedef long int ssize_t;
-typedef unsigned long int size_t;
 
 /* FIXME: use long for ids (requires changing printk prefix) */
 typedef int id_t;
