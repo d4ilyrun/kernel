0f9cbd137dd026c99050d5e4c286fb30f42497d0
Léo DUBOIN <leo@duboin.com>
2025-06-21 18:40:35 +0200

    i686: cpu: add MSR read/write instruction

--- include/kernel/arch/i686/cpu.h
+++ include/kernel/arch/i686/cpu.h
@@ -153,4 +153,22 @@ CPUID_FUNCTION(edx)
 
 #undef CPUID_FUNCTION
 
+
+/* Read from specific register */
+static inline uint64_t rdmsr(uint32_t msr)
+{
+    uint32_t eax;
+    uint32_t edx;
+    ASM("rdmsr" : "=a"(eax), "=d"(edx) : "c"(msr));
+    return (((uint64_t)edx) << 32) | eax;
+}
+
+/* Write into model specific register */
+static inline void wrmsr(uint32_t msr, uint64_t val)
+{
+    uint32_t eax = val;
+    uint32_t edx = val >> 32;
+    ASM("wrmsr" : : "a"(eax), "d"(edx), "c"(msr));
+}
+
 #endif /* KERNEL_I686_UTILS_CPU_OPS_H */
