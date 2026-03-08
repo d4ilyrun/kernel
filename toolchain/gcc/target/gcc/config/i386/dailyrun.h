508a9128a2ea176b0a1c1fec6a32a7a04329d489
Léo DUBOIN <leo@duboin.com>
2026-03-08 20:40:21 +0100

    toolchain/gcc: include our own headers first
    We define our own set of headers inside /usr/lib/dailyrun/include.
    These headers must be included before newlib's own headers so that
    they can replace or add additional definitions using #include_next.

--- toolchain/gcc/target/gcc/config/i386/dailyrun.h
+++ toolchain/gcc/target/gcc/config/i386/dailyrun.h
@@ -7,6 +7,21 @@
 #undef ENDFILE_SPEC
 #define ENDFILE_SPEC "crtend.o%s crtn.o%s"
 
+#define NATIVE_SYSTEM_WRAPPER_HEADER_DIR "/usr/lib/dailyrun/include"
+
+#define INCLUDE_DEFAULTS                                     \
+    {                                                        \
+        { GCC_INCLUDE_DIR, "GCC", 0, 0, 0, 0 },              \
+        { LOCAL_INCLUDE_DIR, 0, 0, 1, 1, 2 },                \
+        { LOCAL_INCLUDE_DIR, 0, 0, 1, 1, 0 },                \
+        { NATIVE_SYSTEM_WRAPPER_HEADER_DIR, 0, 0, 0, 1, 2 }, \
+        { NATIVE_SYSTEM_WRAPPER_HEADER_DIR, 0, 0, 0, 1, 0 }, \
+        { NATIVE_SYSTEM_HEADER_DIR, 0, 0, 0, 1, 2 },         \
+        { NATIVE_SYSTEM_HEADER_DIR, 0, 0, 0, 1, 0 },         \
+        { FIXED_INCLUDE_DIR, "GCC", 0, 0, 0, 0 },            \
+        { 0, 0, 0, 0, 0, 0 },                                \
+    }
+
 #undef TARGET_OS_CPP_BUILTINS
 #define TARGET_OS_CPP_BUILTINS()      \
   do {                                \
