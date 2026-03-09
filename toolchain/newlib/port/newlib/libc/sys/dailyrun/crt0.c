2d522346e3c93efd0a618bd7c3b0e067c9648414
Léo DUBOIN <leo@duboin.com>
2026-03-10 00:11:05 +0100

    toolchain/newlib: configure to use our own POSIX signal implementation
    Also implement BSD's killpg() which is defined but not implemented by newlib.

--- toolchain/newlib/port/newlib/libc/sys/dailyrun/crt0.c
+++ toolchain/newlib/port/newlib/libc/sys/dailyrun/crt0.c
@@ -1,9 +1,32 @@
+#include <dailyrun/../sys/signal.h>
+
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 
 extern int main(int argc, char **argv, char **envp);
 
 extern char _edata;
 extern char _end;
+
+/*
+ * Signal handler trampoline.
+ *
+ * This function calls the actual signal handler and makes sure
+ * the process exits from the signal correctly.
+ *
+ * Must be installed using sigsethandler() first before installing
+ * custom signal handlers.
+ */
+static void handle_signal(int signal, siginfo_t *info, void *data)
+{
+    ucontext_t *ucontext = data;
+    sig_sa_sigaction_t handler = ucontext->sa_handler;
+
+    handler(signal, info, ucontext);
+    sigreturn(ucontext);
+}
 
 void _start(int argc, char **argv, char **envp)
 {
@@ -15,6 +38,15 @@ void _start(int argc, char **argv, char **envp)
     while (bss < &_end)
         *bss++ = 0;
 
+    /* Install signal handler trampoline. */
+    ret = sigsethandler(handle_signal);
+    if (ret) {
+        fprintf(stdout, "crt0: sigsethandler failed: %s\n", strerror(errno));
+        goto out;
+    }
+
     ret = main(argc, argv, envp);
+
+out:
     _exit(ret);
 }
