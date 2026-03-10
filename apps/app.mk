617cef648a8222087b6dbf056a04f4b81a06a8f5
Léo DUBOIN <leo@duboin.com>
2026-03-10 10:42:10 +0100

    apps: make the application specify the binary's path
    Do not force applications to be placed inside /usr/bin as we did
    before.

--- apps/app.mk
+++ apps/app.mk
@@ -10,6 +10,7 @@ $(call ASSERT_DEFINED,SRC_DIR)
 $(call ASSERT_DEFINED,BUILD_DIR)
 $(call ASSERT_DEFINED,INSTALL_DIR)
 $(call ASSERT_DEFINED,APP_NAME)
+$(call ASSERT_DEFINED,APP_EXECUTABLE)
 $(call ASSERT_DEFINED,APP_SOURCES)
 
 APP_OBJS := $(addsuffix .o, $(addprefix $(BUILD_DIR)/,$(APP_SOURCES)))
@@ -20,17 +21,17 @@ CFLAGS += -MMD -MP
 
 all: build
 
-build:   prepare $(BUILD_DIR)/$(APP_NAME)
-install: $(INSTALL_DIR)/bin/$(APP_NAME)
+build:   $(BUILD_DIR)/$(APP_EXECUTABLE)
+install: $(INSTALL_DIR)/$(APP_EXECUTABLE)
 
 prepare:
-	mkdir -p $(BUILD_DIR) $(INSTALL_DIR)
+	mkdir -p $(dir $(BUILD_DIR)/$(APP_EXECUTABLE))
 
-$(INSTALL_DIR)/bin/$(APP_NAME): prepare build
-	install -D -m 755 $(BUILD_DIR)/$(APP_NAME) $(INSTALL_DIR)/bin/$(APP_NAME)
+$(INSTALL_DIR)/$(APP_EXECUTABLE): prepare build
+	install -D -m 755 $(BUILD_DIR)/$(APP_EXECUTABLE) $(INSTALL_DIR)/$(APP_EXECUTABLE)
 
-$(BUILD_DIR)/$(APP_NAME): prepare $(APP_OBJS)
-	$(CC) $(APP_OBJS) -o $(BUILD_DIR)/$(APP_NAME) $(APP_LDFLAGS) $(LDFLAGS)
+$(BUILD_DIR)/$(APP_EXECUTABLE): prepare $(APP_OBJS)
+	$(CC) $(APP_OBJS) -o $@ $(APP_LDFLAGS) $(LDFLAGS)
 
 $(BUILD_DIR)/%.c.o: $(SRC_DIR)/%.c
 	$(CC) $(APP_CPPFLAGS) $(CPPFLAGS) $(APP_CFLAGS) $(CFLAGS) -c $< -o $@
