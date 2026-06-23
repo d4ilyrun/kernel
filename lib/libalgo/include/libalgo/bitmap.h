2df924026d13683dd89b94a61b25936b9b860416
Léo DUBOIN <leo@duboin.com>
2026-06-23 14:54:13 +0200

    libalgo: bitmap: add bitmap_first()/bitmap_last()

--- lib/libalgo/include/libalgo/bitmap.h
+++ lib/libalgo/include/libalgo/bitmap.h
@@ -49,6 +49,16 @@ typedef bitmap_block_t *bitmap_t;
     bitmap_block_t _name[BITMAP_OFFSET(NON_ZERO(_size) - 1) + 1]
 
 /**
+ * Fill a bitmap with zeroes.
+ */
+#define bitmap_fill_zeroes(bitmap) memset(bitmap, 0, sizeof(bitmap));
+
+/**
+ * Fill a bitmap with ones.
+ */
+#define bitmap_fill_ones(bitmap) memset(bitmap, 0xff, sizeof(bitmap));
+
+/**
  * @brief Read the value at a given index inside a bitmap
  *
  * @warning This function does not perform any bound checking
@@ -93,6 +103,120 @@ bitmap_assign(bitmap_t bitmap, uint32_t index, bool value)
     else
         bitmap_clear(bitmap, index);
 }
+
+static ALWAYS_INLINE int __bitmap_first(bitmap_t bitmap, uint32_t size)
+{
+    uint32_t blocks = BITMAP_OFFSET(NON_ZERO(size) - 1) + 1;
+
+    for (uint32_t i = 0; i < blocks; ++i) {
+        bitmap_block_t block = bitmap[i];
+
+        if (block)
+            return i * BITMAP_BLOCK_SIZE + __builtin_ctzl(block);
+    }
+
+    return -1;
+}
+
+static ALWAYS_INLINE int
+__bitmap_first_clear(const bitmap_t bitmap, uint32_t size)
+{
+    uint32_t blocks = BITMAP_OFFSET(NON_ZERO(size) - 1) + 1;
+
+    for (uint32_t i = 0; i < blocks; ++i) {
+        bitmap_block_t block = ~bitmap[i];
+
+        /* Mask off bits beyond the bitmap size in the last block */
+        if (i == blocks - 1) {
+            uint32_t rem = size % BITMAP_BLOCK_SIZE;
+
+            if (rem)
+                block &= (((bitmap_block_t)1 << rem) - 1);
+        }
+
+        if (block)
+            return i * BITMAP_BLOCK_SIZE + __builtin_ctzl(block);
+    }
+
+    return -1;
+}
+
+static ALWAYS_INLINE int __bitmap_last(bitmap_t bitmap, uint32_t size)
+{
+    uint32_t blocks = BITMAP_OFFSET(size - 1) + 1;
+
+    if (!blocks)
+        return -1;
+
+    uint32_t rem = size % BITMAP_BLOCK_SIZE;
+
+    for (uint32_t i = blocks; i-- > 0;) {
+        bitmap_block_t block = bitmap[i];
+
+        if (i == blocks - 1 && rem)
+            block &= (((bitmap_block_t)1 << rem) - 1);
+
+        if (block)
+            return i * BITMAP_BLOCK_SIZE +
+                   (BITMAP_BLOCK_SIZE - 1 - __builtin_clzl(block));
+    }
+
+    return -1;
+}
+
+static ALWAYS_INLINE int
+__bitmap_last_clear(const bitmap_t bitmap, uint32_t size)
+{
+    uint32_t blocks = BITMAP_OFFSET(NON_ZERO(size) - 1) + 1;
+
+    for (uint32_t i = blocks; i-- > 0;) {
+        bitmap_block_t block = ~bitmap[i];
+
+        /* Mask off unused bits in last block */
+        if (i == blocks - 1) {
+            uint32_t rem = size % BITMAP_BLOCK_SIZE;
+
+            if (rem)
+                block &= (((bitmap_block_t)1 << rem) - 1);
+        }
+
+        if (block) {
+            /* position of highest set bit in inverted block */
+            return i * BITMAP_BLOCK_SIZE +
+                   (BITMAP_BLOCK_SIZE - 1 - __builtin_clzl(block));
+        }
+    }
+
+    return -1;
+}
+
+/**
+ * Find the first bit set inside a bitmap.
+ *
+ * @return The bit's index, or -1 if all bits are clear.
+ */
+#define bitmap_last(bitmap) __bitmap_last(bitmap, sizeof(bitmap));
+
+/**
+ * Find the last bit set inside a bitmap.
+ *
+ * @return The bit's index, or -1 if all bits are clear.
+ */
+#define bitmap_first(bitmap) __bitmap_first(bitmap, sizeof(bitmap));
+
+/**
+ * Find the first bit set inside a bitmap.
+ *
+ * @return The bit's index, or -1 if all bits are clear.
+ */
+#define bitmap_last_clear(bitmap) __bitmap_last_clear(bitmap, sizeof(bitmap));
+
+/**
+ * Find the last bit set inside a bitmap.
+ *
+ * @return The bit's index, or -1 if all bits are clear.
+ */
+#define bitmap_first_clear(bitmap) __bitmap_first_clear(bitmap, sizeof(bitmap));
 
 /** @} */
 
