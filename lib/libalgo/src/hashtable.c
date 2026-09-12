2514f4188fe6745ffdc614d6c7b7b807a9be65fa
Léo DUBOIN <leo@duboin.com>
2026-09-12 12:27:43 +0200

    libalgo/hashtable: allow using different hash/comparison functions
    This will make it possible to use this structure with different
    types of keys.

--- lib/libalgo/src/hashtable.c
+++ lib/libalgo/src/hashtable.c
@@ -9,7 +9,7 @@
  *
  * http://web.archive.org/web/20071223173210/http://www.concentric.net/~Ttwang/tech/inthash.htm
  */
-static uint32_t hash32(uint32_t a)
+u32 hash32(uint32_t a)
 {
 	a = (a + 0x7ed55d16) + (a << 12);
 	a = (a ^ 0xc761c23c) ^ (a >> 19);
@@ -22,37 +22,54 @@ static uint32_t hash32(uint32_t a)
 }
 
 /*
- * Compute the hash value of an entry's key.
+ * Hashing function used when the key is a memory address.
+ */
+u32 hash_address(const void *key)
+{
+#if ARCH_WORD_SIZE == 4
+	return hash32((u32)key);
+#elif ARCH_WORD_SIZE == 8
+	/* Too lazy to implement a real 64b hash function, so we perform a 32b hash
+	 * of the lower 32bits. */
+	return hash32((u64)key);
+#else
+#error No pointer hash function for this architecture.
+#endif
+}
+
+/*
+ * Compute the hash of an entry's key.
  *
  * @return The entry's index into the hashtable's list of buckets.
  */
 static inline unsigned long hash_key(struct hashtable *table, const void *key)
 {
-#if ARCH_WORD_SIZE == 4
-	return hash32((u32)key) % table->size;
-#elif ARCH_WORD_SIZE == 8
-	/* Too lazy to implement a real 64b hash function, so we perform a 32b hash
-	 * of the lower 32bits. */
-	return hash32((u64)key) % table->size;
-#else
-#error No hash function for this architecture.
-#endif
+	return table->hash_func(key) % table->size;
+}
+
+int compare_addresses(const void *left, const void *right)
+{
+	return (left == right) ? COMPARE_EQ : !COMPARE_EQ;
 }
 
 /*
  * Compare an hash entry against a given key for lookup.
  */
-static inline bool hash_entry_is(const struct hashtable_entry *entry, const void *key)
+static inline bool
+hash_entry_is(const struct hashtable *table, const struct hashtable_entry *entry, const void *key)
 {
-	return hashtable_entry_key(entry) == key;
+	return table->compare_key(hashtable_entry_key(entry), key) == COMPARE_EQ;
 }
 
 /*
  *
  */
-void __hashtable_init(struct hashtable *table, size_t size)
+void __hashtable_init(struct hashtable *table, size_t size, hash_func_t hash_func,
+		      compare_t compare_key)
 {
 	table->size = size;
+	table->hash_func = hash_func;
+	table->compare_key = compare_key;
 
 	for (size_t i = 0; i < size; ++i)
 		INIT_LLIST(table->buckets[i]);
@@ -76,7 +93,7 @@ struct hashtable_entry *____hashtable_find(struct hashtable *table, const void *
 
 	bucket = &table->buckets[hash_key(table, key)];
 	FOREACH_LLIST_ENTRY (entry, bucket, this) {
-		if (hash_entry_is(entry, key)) {
+		if (hash_entry_is(table, entry, key)) {
 			if (remove)
 				llist_remove(&entry->this);
 			break;
