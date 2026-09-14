aa73ee7e6e987fdd73ef6c31f3b6261d0aa1c026
Léo DUBOIN <leo@duboin.com>
2026-09-14 16:28:08 +0200

    net/route: fix invalid source port
    We were hardcoding the source port to be the same as the destination
    one ... Why?

--- kernel/net/route.c
+++ kernel/net/route.c
@@ -9,9 +9,14 @@ error_t net_route_compute(struct net_route *route, const struct sockaddr_in *dst
 	const struct subnet *subnet;
 	const mac_address_t *daddr_mac;
 
+	/* IPv6 not supported */
+	if (dst->sin_family != AF_INET)
+		return E_NET_UNREACHABLE;
+
 	subnet = net_interface_find_subnet(dst->sin_addr.s_addr);
 	if (!subnet)
 		return E_NET_UNREACHABLE;
+	route->netdev = subnet->interface->netdev;
 
 	daddr_mac = arp_get(dst->sin_addr.s_addr);
 	if (!daddr_mac) {
@@ -20,13 +25,10 @@ error_t net_route_compute(struct net_route *route, const struct sockaddr_in *dst
 			return E_NET_UNREACHABLE;
 	}
 
-	route->netdev = subnet->interface->netdev;
-
-	route->dst.ip = *dst;
+	memcpy(&route->dst.ip, dst, sizeof(route->dst.ip));
 	memcpy(route->dst.mac.mac_addr, daddr_mac, sizeof(mac_address_t));
 
-	/* Use same port and family for source */
-	route->src.ip = *dst;
+	route->src.ip.sin_family = dst->sin_family;
 	route->src.ip.sin_addr.s_addr = subnet->ip;
 	memcpy(route->src.mac.mac_addr, subnet->interface->netdev->mac, sizeof(mac_address_t));
 
